346089f143050a583cf56c3e744e0c9180c743ed
Khafid Prayoga <khafidp@pm.me>
2021-12-02 10:10:22 +0700

    edit script order, and change blockies bband color

--- src/utils/avatar.js
+++ src/utils/avatar.js
@@ -1,8 +1,8 @@
 let icon = blockies.create({
   seed: '0x0000000000000000000000000000000000000000',
-  color: '#999', // to manually specify the icon color, default: random
-  bgcolor: '#37cdbe', // choose a different background color, default: random
-  spotcolor: '#2aa79b',
+  color: '#60A5FA', // choose a different background color, default: random
+  bgcolor: '#A7F3D0', // to manually specify the icon color, default: random
+  spotcolor: '#111827',
   size: 10,
 });
 
