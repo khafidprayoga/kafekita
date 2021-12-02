bd9a55954775f00d5aa32380f8d18404f384c5fd
Khafid Prayoga <khafidp@pm.me>
2021-12-02 08:31:38 +0700

    integrate daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="font-bold text-4xl m-5 text-red-600 underline">
+    <div className="font-bold text-4xl m-5 text-red-600 underline font-brand">
       Hello World
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
+const daisyui = require("daisyui");
+
 module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        brand: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"],
+  },
 };
