e77feb8efd9846a092436b6ac89f6a22a658ec09
Khafid Prayoga <khafidp@pm.me>
2021-12-05 21:16:44 +0700

    add factory method from dacade

--- src/main.js
+++ src/main.js
@@ -1,76 +1,86 @@
 import "./tailwind.css";
 import "ethereum-blockies/blockies";
 import "lazysizes";
+import { newKitFromWeb3, web3 } from "@celo/contractkit";
+import BigNumber from "bignumber.js";
+import kafekitaAbi from "./contracts/KafeKita.abi.json";
+import cUSDAbi from "./contracts/cUSDInterface.abi.json";
 
-const products = [
-  {
-    name: "Lemonice",
-    imageURL:
-      "https://bafybeibhyjof4pkotj6ipmoidjxeip7xupm7g6zl3r6mcmclcm7zu2y4te.ipfs.dweb.link/1638417959956-937-7b483f5889d128e8",
-    description: `Fit for you who are thirsty after exercise, with a blend of ice from Antarctica + Lemonice.`,
-    price: 3,
-    sold: 10,
-  },
-  {
-    name: "Javanesso",
-    imageURL:
-      "https://bafybeieuqiteflq5awbur3xx47yek5idqyjxzjsaqdenhgsl7ratttvyr4.ipfs.dweb.link/1638418018770-937-8bf78859b80b03aa",
-    description: `Have you traveled to Borobudur temple?`,
-    price: 5,
-    sold: 10,
-  },
-  {
-    name: "Fruizzy",
-    imageURL:
-      "https://bafybeiawbzvorjmc5yqxjndpdf4rulh4ivnxj57df3rrhgayknojyrjywa.ipfs.dweb.link/1638418056598-937-818b86762564eb97",
-    description: `Have you traveled to Borobudur temple?`,
-    price: 5,
-    sold: 10,
-  },
-  {
-    name: "Monggo",
-    imageURL:
-      "https://bafybeig2fvkrdbwv2dlyxhhwawwge3d4qonfn4zaggbpc4azyyomm3woeu.ipfs.dweb.link/1638418095735-937-f11e1115bd1650dd",
-    description: `Tropical fruit`,
-    price: 4,
-    sold: 0,
-  },
-  {
-    name: "YogHurt",
-    imageURL:
-      "https://bafybeicbbtgi2wkpxxeskjd4lbq4a4yqbc5pc4dsiuf5lftyfpfdacl3xi.ipfs.dweb.link/1638418157261-937-a926364639685a8e",
-    description: `How about your childhood?`,
-    price: 5,
-    sold: 100,
-  },
-  {
-    name: "Chocolatey",
-    imageURL:
-      "https://bafybeieqkpi4zl4tsp2qhnze25eploz3dbypijegvngx3mkhw223n7o3t4.ipfs.dweb.link/1638418211376-937-a18b4f644f609757",
-    description: `I think, its a package manager for Windows.`,
-    price: 1,
-    sold: 20,
-  },
-  {
-    name: "Avoicado",
-    imageURL:
-      "https://bafybeia6db4cnwrv5ox3uo3wco4gyqi7xivg2dg5gon2gvzatj32maek2a.ipfs.dweb.link/1638418264770-937-c14adf9363dd0d68",
-    description: `don't run away from the problem`,
-    price: 2,
-    sold: 10,
-  },
-];
+const ERC20_DECIMALS = 18;
+const KafeKitaAddress = "0xa3b48a85DE80D87A014514ac393d3E9f489732CE";
+const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
-const getBalance = () => {
-  document.querySelector("#balance").textContent = 20;
+let kit;
+let contract;
+let products = [];
+
+const connectCeloWallet = async () => {
+  if (window.celo) {
+    notification("⚠️ Please approve this dApp to use it.");
+    try {
+      await window.celo.enable();
+
+      const web3 = new web3(window.celo);
+      kit = newKitFromWeb3(web3);
+
+      const accounts = await kit.web3.eth.getAccounts();
+      kit.defaultAccount = accounts[0];
+      contract = new kit.web3.eth.Contract(kafekitaAbi, KafeKitaAddress);
+    } catch (err) {
+      notification(`⚠️ ${err}.`);
+    }
+  } else {
+    notification("⚠️ Please install the CeloExtensionWallet.");
+  }
 };
 
-const getTX = () => {
-  document.querySelector("#user-tx").textContent = 1;
+const getBalance = async function () {
+  const totalBalance = await kit.getTotalBalance(kit.defaultAccount);
+  const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2);
+  document.querySelector("#balance").textContent = cUSDBalance;
+};
+
+const getProducts = async () => {
+  const _productsLength = await contract.methods.getTotalDrinks().call();
+  const _products = [];
+  for (let i = 0; i < _productsLength; i++) {
+    let _product = new Promise(async (resolve, reject) => {
+      let product = await contract.methods.getDrinkDetails(i).call();
+      console.log(product);
+      resolve({
+        id: i,
+        owner: product[0],
+        name: product[1],
+        imageURL: product[2],
+        description: product[3],
+        price: new BigNumber(product[4]),
+        sold: product[5],
+      });
+    });
+    _products.push(_product);
+  }
+  products = await Promise.all(_products);
+  renderProducts();
+};
+
+const getTX = async () => {
+  const userTx = await contract.methods.getAllTX().call();
+  document.querySelector("#user-tx").textContent = userTx;
+};
+
+const approve = async (_price) => {
+  const cUSDContract = new kit.web3.eth.Contract(cUSDAbi, cUSDContractAddress);
+
+  const bill = await cUSDContract.methods
+    .approve(KafeKitaAddress, _price)
+    .send({ from: kit.defaultAccount });
+
+  return bill;
 };
 const notification = (_msg) => {
   document.querySelector("#notification").textContent = _msg;
 };
+
 const renderDrinks = () => {
   document.querySelector("#marketplace").innerHTML = "";
   products.forEach((_product, index) => {
@@ -152,32 +162,50 @@ const productTemplate = (_product, _id) => {
 `;
 };
 
-document.querySelector("#marketplace").addEventListener("click", (event) => {
-  if (event.target.className.includes("btn-success")) {
-    const btnBuy = event.target.id.split("-");
-    const id = Number.parseInt(btnBuy[2]);
+document
+  .querySelector("#marketplace")
+  .addEventListener("click", async (event) => {
+    if (event.target.className.includes("btn-success")) {
+      const btnBuy = event.target.id.split("-");
+      const id = Number.parseInt(btnBuy[2]);
 
-    const inputQty = Number.parseInt(
-      document.querySelector(`#product-qty-${id}`).value
-    );
+      const inputQty = Number.parseInt(
+        document.querySelector(`#product-qty-${id}`).value
+      );
 
-    const qty =
-      inputQty < 1 || inputQty > 10 ? "error" : Number.parseInt(inputQty);
+      const qty =
+        inputQty < 1 || inputQty > 10 ? "error" : Number.parseInt(inputQty);
 
-    if (qty !== "error") {
-      products[id].sold += qty;
-      notification(`Processing your ${qty} of ${products[id].name}`);
-    } else {
-      notification("An error occured");
+      notification("⌛ Waiting for payment approval...");
+      if (qty !== "error") {
+        try {
+          await approve(products[id].price * qty);
+          notification(`⌛ Awaiting payment for "${products[id].name}"...`);
+        } catch (err) {
+          notification(`${err}`);
+        }
+
+        try {
+          const result = await contract.methods
+            .buyItem(id, qty)
+            .send({ from: kit.defaultAccount });
+          notification(
+            `⌛ Processing tx "${products[id].name}" ${qty} item...`
+          );
+          renderDrinks();
+          await getBalance();
+        } catch (err) {}
+      } else {
+        notification("An error occured");
+      }
     }
+  });
 
-    renderDrinks();
-  }
-});
-
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
   notification("Initializing....");
-  getBalance();
-  getTX();
+  await connectCeloWallet();
+  await getBalance();
+  await getProducts();
+  await getTX();
   renderDrinks();
 });
